af32e9b29dec22643eb0e2954278c88bae638b5a
shivamvermaiitian <157829350+shivamvermaiitian@users.noreply.github.com>
2024-04-08 13:54:42 +0000

    num

---
+++ start/num.js
@@ -0,0 +1,32 @@
+// const score=400;
+// const balance = new Number (939);
+// console.log(score);
+// console.log(balance);
+// console.log(typeof balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 123.4489;
+// console.log(typeof otherNumber.toPrecision(4));
+// // toPrecision return string
+
+// const hundreds=1000000;
+// console.log(hundreds.toLocaleString());// us standard
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// console.log(Number.MAX_SAFE_INTEGER);
+
+// number property can be used for different property
+
+
+// Math library
+console.log(Math);
+console.log(Math.sqrt(25));
+console.log(Math.floor(45.0899));
+console.log(Math.min(34,35,5));
+console.log(Math.random().toFixed(9));
+// number between 10 and 20
+console.log(10+10*Math.random().toFixed(1));
+let min=10
+let max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min);
