7de9c5d2f84524e78f867cf8bb268f7def95b2a8
shivamvermaiitian <157829350+shivamvermaiitian@users.noreply.github.com>
2024-04-08 07:37:36 +0000

    conversion

---
+++ start/test4.js
@@ -0,0 +1,16 @@
+let score =undefined;
+let valueInNumber= Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+//value in number does not properly due to letter and digit
+//if score is null then zero is output if undefined then Nan
+//true 1 false 0
+let isLoggedIn="shiva";
+let booleanisLoggedIn=Boolean(isLoggedIn);
+console.log(booleanisLoggedIn);
+//empty string give 0 string give true in boolean
+let number=903;
+let stringNumber=String(number);
+
+console.log(typeof stringNumber);
+console.log(stringNumber);
---
+++ start/test5.js
@@ -0,0 +1,12 @@
+//oprations
+// console.log(2**3);
+// let str1="shivam";
+// let str2=" verma";
+// console.log(str1+str2);
+
+console.log(1+"3");
+console.log("1"+2);
+console.log(1+1+"2");
+console.log(1+1+"2"+3+3);
+console.log(+true);
+console.log(+"");
