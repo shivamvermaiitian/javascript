2c954c672f9c92133780a5f0fd2d3dc35fcbac57
shivamvermaiitian <157829350+shivamvermaiitian@users.noreply.github.com>
2024-04-09 13:27:38 +0000

    json

---
+++ start/object2.js
@@ -0,0 +1,38 @@
+// destructuring object
+
+const course = {
+    name:"js in hindi",
+    courseInstructure:"jitesh",
+    pay: "999",
+}
+
+const {courseInstructure : instructure} = course;//destructure
+console.log(instructure);
+
+//api json= {    }
+
+// {
+//     "name":"hitesh",
+//     "price":"free",
+// }
+
+// api is array or object
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
