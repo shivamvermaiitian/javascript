966d6e05eb177bb7ed0c45542f9a696cfd4d9b06
shivamvermaiitian <157829350+shivamvermaiitian@users.noreply.github.com>
2024-04-13 16:09:08 +0000

    loop

---
+++ start/iterations/loops.js
@@ -0,0 +1,55 @@
+// for(let r=1;r<10;++r)console.log(r);
+// const arr = [ 2, 3,4,,]
+// for (let r = 0; r < arr.length; r++) {
+//     // const element = arr[r];
+//     console.log(arr[r]); 
+// }
+
+// for (let i = 1; i < 11; i++) {
+//     for (let j = 1; j < 11; j++) {
+//         // console.log(`${i} * ${j} = ${i*j}`);
+//         console.log(i+"*"+j+"="+i*j);// add string in console
+//     }
+// }
+
+// continue and break
+// continue skip one step and break apply to stop loop further
+let count =0
+// while(count<20){
+//     console.log(`value of count is ${count}`)
+//     count+=2
+// }
+
+do {
+    console.log(`value of count is ${count}`)
+    count+=2
+} while (count<=20);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
